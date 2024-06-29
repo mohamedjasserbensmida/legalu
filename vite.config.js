49394bd5bf02bc03332aba00be881da031ec5254
mohamedjasserbensmida <74205089+mohamedjasserbensmida@users.noreply.github.com>
2024-06-29 13:12:24 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import externals from 'vite-plugin-externals';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['three'], // Exclude 'three' package
+  base: './', // Assurez-vous que le chemin de base est correct
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+        },
+      },
+      chunkSizeWarningLimit: 1000, // Ajustez la limite de taille des chunks
+    },
   },
-  base: 'https://tpspace.github.io/3D-Portfolio/'
-})
+});
