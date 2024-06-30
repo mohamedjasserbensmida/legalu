2d3979ef6f266bc77c674eac05b69783433edc6d
mohamedjasserbensmida <74205089+mohamedjasserbensmida@users.noreply.github.com>
2024-06-30 16:22:46 +0100

    last upd

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -31,7 +31,7 @@ const Contact = () => {
     e.preventDefault();
     setLoading(true);
 
-    emailjs
+    /*emailjs
       .send(
         import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
@@ -61,7 +61,7 @@ const Contact = () => {
 
           alert("Ahh, something went wrong. Please try again.");
         }
-      );
+      );*/
   };
 
   return (
